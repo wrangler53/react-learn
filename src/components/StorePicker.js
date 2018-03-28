a82846b5e48bcbae22ee93be37e355d351f8bb2d
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-03-28 18:09:01 +0300

    Added onsubmit form and default value of input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 export default class StorePicker extends React.Component {
+    goToStore = (event) => {
+        event.preventDefault();
+    }
+
     render() {
         return(
-            <form action="" className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please, enter a store name</h2>
-                <input type="text" placeholder="Store name" required/>
+                <input type="text" placeholder="Store name" defaultValue={getFunName()} required/>
                 <input type="submit" value="Submit"/>
             </form>
         ) 
