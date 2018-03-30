8546f7d0afbb774046365eee1e03adaa7e35513e
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-03-30 17:20:59 +0300

    Render page using JSX

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes'
+import Fish from './Fish';
 
 export default class App extends React.Component {
 
@@ -32,6 +33,13 @@ export default class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {
+                            Object.keys(this.state.fishes).map(key => 
+                                <Fish key={key} details={this.state.fishes[key]} />
+                            )
+                        }
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
---
+++ src/components/Fish.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+export default class Fish extends React.Component {
+    render() {
+
+        const { name, image, price, desc, avaliability } = this.props.details;
+
+        return(
+            <li className="menu-fish">
+                <img src={image} alt={name} />
+                <h3 className="fish-name">
+                    {name}
+                    <span className="price">
+                        {formatPrice(price)}
+                    </span>
+                </h3>
+                <p>{desc}</p>
+                <button>Add To Cart</button>
+            </li>
+        );
+    }
+}
